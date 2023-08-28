7374f7a5994bc0743e398822d02fdaa35c62bf9a
Tavkel <gmininb@gmail.com>
2023-08-28 20:06:37 +0300

    Added forgotten Controller, small path fix

---
+++ src/main/java/com/example/course02/controllers/MathQuestionController.java
@@ -0,0 +1,41 @@
+package com.example.course02.controllers;
+
+import com.example.course02.models.Question;
+import com.example.course02.services.interfaces.QuestionService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping(path = "/math")
+public class MathQuestionController {
+    private final QuestionService questionService;
+
+    public MathQuestionController(@Qualifier("mathService") QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @GetMapping(path = "/add")
+    public ResponseEntity<Question> addQuestion(@RequestParam String question, @RequestParam String answer) {
+        var result = questionService.add(question, answer);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Collection<Question>> getAllQuestions() {
+        return new ResponseEntity<>(questionService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/remove")
+    public ResponseEntity<Question> removeQuestion(@RequestParam String question, @RequestParam String answer) {
+        var result = new Question(question, answer);
+        questionService.remove(result);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+}
